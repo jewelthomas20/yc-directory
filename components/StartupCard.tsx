b9398c38ab5f1fcbc6dfd790be97cc2bf7deeb78
Jewel <jewelthomas@Jewels-MacBook-Air.local>
2025-09-22 02:10:45 +0530

    live fetch - setted up sanity schemas - fetched live data on demand and cacheing tested

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -4,9 +4,12 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
+import { Author, Startup } from '@/sanity/types'
+
+export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
 const StartupCard = ({post}:{post:StartupTypeCard}) => {
-    const {_createdAt, views, author:{_id:authorId, name}, _id, description, title, category, image} = post
+    const {_createdAt, views, author, _id, description, title, category, image} = post
   return (
     <li className='startup-card group'>
         <div className='flex-between'>
@@ -20,15 +23,15 @@ const StartupCard = ({post}:{post:StartupTypeCard}) => {
         </div>
         <div className='flex-between mt-5 gap-5'>
             <div className='flex-1'>
-                <Link href={`/user/${authorId}`}>
-                <p className='text-16-medium line-clamp-1'>{name}</p>
+                <Link href={`/user/${author?._id}`}>
+                <p className='text-16-medium line-clamp-1'>{author?.name}</p>
                 </Link>
                 <Link href={`/startup/${_id}`}>
                 <h3 className='text-26-semibold line-clamp-1'>{title}</h3>
                 </Link>
             </div>
-            <Link href={`/user/${authorId}`}>
-            <Image src={'https://placehold.co/600x400'} alt={title} width={48} height={48} className='rounded-full'/>
+            <Link href={`/user/${author?._id}`}>
+            <Image src={'https://placehold.co/600x400'} alt={title+''} width={48} height={48} className='rounded-full'/>
             </Link>
         </div>
         <Link href={`/startup/${_id}`}>
@@ -36,7 +39,7 @@ const StartupCard = ({post}:{post:StartupTypeCard}) => {
         <img src={image} alt={title} className='startup-card_img'/>
         </Link>
         <div className='flex-between gap-3 mt-5'>
-            <Link href={`/?query=${category.toLowerCase()}`}>
+            <Link href={`/?query=${category?.toLowerCase()}`}>
             <p className='text-16-medium'>{category}</p>
             </Link>
             <Button className='startup-card_btn' asChild>
